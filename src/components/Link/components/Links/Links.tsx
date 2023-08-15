ae3a45266ccf5dc970f9e3370dc3231109f7f88f
Edwin León <edwin1224leon@outlook.com>
2023-08-14 19:32:33 -0600

    fix: return type issue on build

--- src/components/Link/components/Links/Links.tsx
+++ src/components/Link/components/Links/Links.tsx
@@ -1,7 +1,11 @@
 import { Link, ILinksProps } from "components";
 
 export const Links = ({ routes, style }: ILinksProps) => {
-  return routes.map((route) => (
-    <Link key={route.id} to={route.to} style={style} name={route.name} />
-  ));
+  return (
+    <>
+      {routes.map((route) => (
+        <Link key={route.id} to={route.to} style={style} name={route.name} />
+      ))}
+    </>
+  );
 };
